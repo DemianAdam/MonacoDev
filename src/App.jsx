827bf1bcd380598c69f7be3548c1594ac5ad725d
Demian <61812941+DemianAdam@users.noreply.github.com>
2024-12-30 02:09:39 -0300

    added-spiner-and-modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,30 @@ import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
-
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
+import RegistrationModal from './components/RegistrationModal/RegistrationModal';
+import { Button } from 'react-bootstrap';
 function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false);
+  const [modalShow, setModalShow] = useState(false)
 
   return (
     <Container className='d-flex flex-column'>
       <Row className='mb-5'>
         <h1>¡Registrate en nuestra Lista!</h1>
       </Row>
-      <Row>
-        <RegistrationForm />
+      <Row className='d-flex justify-content-center'>
+        {
+          loading ? <LoadingSpinner /> :
+            <RegistrationForm onLoading={setLoading} setModalShow={setModalShow} />
+        }
+        <RegistrationModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
       </Row>
+      {/*<Button onClick={()=>setModalShow(true)}>asd</Button>*/}
     </Container>
 
   )
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -7,7 +7,8 @@ import BirthDateInput from '../BirthDateInput/BirthDateInput';
 import axios from 'axios';
 
 
-export default function RegistrationForm() {
+
+export default function RegistrationForm({ onLoading, setModalShow }) {
     const [errorMessage, setErrorMessage] = useState("");
     const [isAgeValid, setIsAgeValid] = useState(true);
     const handleAgeValidityChange = (validity) => {
@@ -15,49 +16,61 @@ export default function RegistrationForm() {
         setErrorMessage("");  // Clear error message when age is valid
     };
     const url = "https://script.google.com/macros/s/AKfycbyOCy6uvx0oOp0H1CWGC6kSlxSJ2uJeEU_OSzMqS6fbtNsFHp1DL3YD-_8gD76nWZxb0A/exec"
-    
+
     const handleSumbit = async (event) => {
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+        event.preventDefault();
+        event.target.reset();
 
         if (!isAgeValid) {
             setErrorMessage("Debes ser mayor de 18 años para registrarte.");
             return;
         }
 
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData.entries());
-        
-        console.log(data)
-
         try {
+
+            onLoading(true)
             const response = await axios.get(url, {
                 params: data,
             });
+
+
             if (response.status === 200) {
-                console.log("asd")
+                setModalShow(true)
+
             }
         } catch (error) {
-         console.log(error)   
+            console.log(error)
+        }
+        finally {
+            onLoading(false);
         }
     };
-    
+
     return (
-        <Form onSubmit={handleSumbit}>
-            <Form.Group className="mb-3" controlId="input-firstname">
-                <Form.Label>Nombre</Form.Label>
-                <Form.Control required minLength={3} maxLength={30} type="text" placeholder="Tu Nombre" name='nombre' />
-            </Form.Group>   
+        <>
 
-            <Form.Group className="mb-3" controlId="input-lastname">
-                <Form.Label>Apellido</Form.Label>
-                <Form.Control required minLength={3} maxLength={30} type="text" placeholder="Tu Apellido" name='apellido' />
-            </Form.Group>
+            <Form onSubmit={handleSumbit}>
+                <Form.Group className="mb-3" controlId="input-firstname">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control required minLength={3} maxLength={30} type="text" placeholder="Tu Nombre" name='nombre' />
+                </Form.Group>
 
-            <DniInput />
-            <BirthDateInput onAgeValidityChange={handleAgeValidityChange} />
-            {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
-            <Button variant="primary" type="submit">
-                Registrate
-            </Button>
-        </Form>
+                <Form.Group className="mb-3" controlId="input-lastname">
+                    <Form.Label>Apellido</Form.Label>
+                    <Form.Control required minLength={3} maxLength={30} type="text" placeholder="Tu Apellido" name='apellido' />
+                </Form.Group>
+
+                <DniInput />
+                <BirthDateInput onAgeValidityChange={handleAgeValidityChange} />
+                {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
+                <Button variant="primary" type="submit">
+                    Registrate
+                </Button>
+                
+            </Form>
+
+        </>
     )
 }
