22355a57a955a51e0e1e97dcb4729ffbbef1da9e
Demian <61812941+DemianAdam@users.noreply.github.com>
2025-01-06 14:19:11 -0300

    testDeploy

--- src/components/Hero/RegisterList/RegisterList.jsx
+++ src/components/Hero/RegisterList/RegisterList.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 export default function RegisterList({ setHeader, setLoading, setModalShow, setModalContent, user, setUser }) {
 
   useEffect(() => {
-    setHeader(<>{`! Hola ${user.name} !`}<p>Hace tu lista de personas</p></>);
+    setHeader(<>{`! Hola ${user.name} wea!`}<p>Hace tu lista de personas</p></>);
   }, [setHeader]);
 
 
