80fa14c261c29ad860f9d83cec24bf2adb3e0f80
Demian <61812941+DemianAdam@users.noreply.github.com>
2025-03-06 22:00:45 -0300

    fix: update header title to include 'TEST' for development purposes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <h1 className='abril-fatface-regular display-1'>MONACO</h1>
+    <h1 className='abril-fatface-regular display-1'>MONACO TEST</h1>
   )
 }
